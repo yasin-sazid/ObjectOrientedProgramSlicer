ab4d5e938ad661f90e1763764ca825702431c02c
bsse1006 <bsse1006@iit.du.ac.bd>
2022-12-03 04:48:09 +0600

    Failed attempt at graph

---
+++ src/smallSourcePackage/Main.java
@@ -0,0 +1,14 @@
+package smallSourcePackage;
+
+public class Main
+{
+    public static void main(String[] args) {
+        int a = 6;
+
+        System.out.println(a);
+
+        a = a + 4;
+
+        System.out.println(a);
+    }
+}
